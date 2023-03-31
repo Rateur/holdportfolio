8d70c071ab449da69533df4b87b67aee5b8f84c7
valentinperies <valentinperies@leschassagnes.net>
2023-03-31 16:20:42 +0200

    new version comming

--- loader.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector('.loader');
-
-window.addEventListener('load', () => {
-
-    loader.classList.add('fondu-out');
-    document.querySelector('.loader').style.display = 'none';
-document.querySelector('.lettre').style.display = 'none';
-
-})
