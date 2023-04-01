32204b9fa629189c5ef54a5fef698345e8989800
valentinperies <valentinperies@leschassagnes.net>
2023-04-01 18:14:47 +0200

    portfolio terminé

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,10 +30,24 @@ modalCloses.forEach((mc) => {
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active work */ 
+const linWork = document.querySelectorAll('.work__item')
 
+function activeWork() {
+    linWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
